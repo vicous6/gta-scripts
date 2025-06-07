94279a1299490cc60635e9a8df253f85508014aa
Victor <vicous@vicous.fr>
2025-06-07 14:11:33 +0200

    f

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './index.css';
-// import './App.css';
 import './App.scss';
 function App() {
   const [positions, setPositions] = useState([]);
@@ -29,46 +26,17 @@ function App() {
     return () => window.removeEventListener('message', handleNuiMessage);
   }, []);
 
-  const handleDelete = (index) => {
-    const updated = [...positions];
-    updated.splice(index, 1);
-    setPositions(updated);
-    // Optional: send to backend here
-  };
 
   return (
     <>
-    <span
-  class="inline-flex divide-x divide-gray-300 overflow-hidden rounded border border-gray-300 bg-white shadow-sm dark:divide-gray-600 dark:border-gray-600 dark:bg-gray-800"
->
-  <button
-    type="button"
-    class="px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:relative dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white"
-  >
-    Edit
-  </button>
 
-  <button
-    type="button"
-    class="px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:relative dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white"
-  >
-    View
-  </button>
-
-  <button
-    type="button"
-    class="px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:relative dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-white"
-  >
-    Delete
-  </button>
-</span>
       <button className="close-button !text-yellow-50" onClick={() =>
         fetch(`https://${GetParentResourceName()}/closeUI`, { method: 'POST' })
       }>
         Close
       </button>
     
-      <div id="container" className="container">
+      {/* <div id="container" className="container">
         <ul id="positions" className="positions-list">
           {positions.map((pos, index) => (
             <li key={index} className="position-item">
@@ -81,7 +49,7 @@ function App() {
             </li>
           ))}
         </ul>
-      </div>
+      </div> */}
     </>
   );
 }
